fcfa1833e8de48bc2486ce408ae4686ac9b62e27
rajeev ranjan <talegarry1202@gmail.com>
2020-05-24 17:06:10 +0530

    make a folder structure for project and finished styling menuitem component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import HomePage from './pages/homepage/homePageComponent'
 
 function App() {
   return (
-    <div className='flex-container'>
-      <div className='menu-item'>
-      <div className= 'content'>
-      <h1 className='title'>Hats</h1>
-      <span className='subtitle'>Shop now</span>
-    </div>
-    </div>
-
-    <div className='menu-item'>
-      <div className= 'content'>
-      <h1 className='title'>Jackets</h1>
-      <span className='subtitle'>Shop now</span>
-    </div>
-    </div>
-
-    <div className='menu-item'>
-      <div className= 'content'>
-      <h1 className='title'>Sleepers</h1>
-      <span className='subtitle'>Shop now</span>
-    </div>
-    </div>
-
-    <div className='menu-item'>
-      <div className= 'content'>
-      <h1 className='title'>Mens</h1>
-      <span className='subtitle'>Shop now</span>
-    </div>
-    </div>
-
-    <div className='menu-item'>
-      <div className= 'content'>
-      <h1 className='title'>Womens</h1>
-      <span className='subtitle'>Shop now</span>
-    </div>
-    </div>
-
-   
-    </div>
-  );
+    <div>
+  <HomePage/>
+  </div>
+    )
 }
 
 export default App;
